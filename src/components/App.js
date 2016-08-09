0c401456a0baeed7fa550223b5cf0f44977a5f99
Denis Koltsov <denis_koltsov@yahoo.com>
2016-08-09 14:22:57 -0400

    urban component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Nav from './Nav.js';
 import Main from './Main.js';
-import util from '../util/helpers.js'
+
 import '../css/App.css';
 
 class App extends Component {
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import util from '../util/helpers.js'
+
 import '../css/Video.css';
 
 class Video extends Component {
