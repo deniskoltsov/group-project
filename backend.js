51dc36cd3c260841df5c44b143e8d344f5d5fe9f
Hiro Narita <hiro.narita@gmail.com>
2016-08-12 15:42:05 -0400

    heroku

--- backend.js
+++ backend.js
@@ -17,14 +17,14 @@ app.get('*', (req, res) =>
 )
 
 app.post('/searchtrack', (req, res) => {
-  const url = 'http://api.musixmatch.com/ws/1.1/matcher.track.get?q_track=' + req.body.data.song + '&q_artist=' + req.body.data.artist + '&apikey=262ed6e47e180f3fe28f5b2e621a5a3f'
+  const url = 'https://api.musixmatch.com/ws/1.1/matcher.track.get?q_track=' + req.body.data.song + '&q_artist=' + req.body.data.artist + '&apikey=262ed6e47e180f3fe28f5b2e621a5a3f'
   request(url, (err, response, body) => {
    res.send(body);
   })
 })
 
 app.post('/searchlyrics', (req, res) => {
-  const url = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + req.body.data.track_id + '&apikey=262ed6e47e180f3fe28f5b2e621a5a3f';
+  const url = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + req.body.data.track_id + '&apikey=262ed6e47e180f3fe28f5b2e621a5a3f';
   request(url, (err, response, body) => {
     res.send(body);
   })
