dcf1d5e7a56fb90c50b6c2e7213fe81f56fbccd4
Denis Koltsov <denis_koltsov@yahoo.com>
2016-08-13 15:18:23 -0400

    more UI

--- src/util/utils-watson.js
+++ src/util/utils-watson.js
@@ -8,7 +8,7 @@ export default {
         input: input
       }
     }
-    return fetch('https://watson-api-explorer.mybluemix.net/text-to-speech/api/v1/synthesize?accept=audio%2Fogg%3Bcodecs%3Dopus&voice=en-US_MichaelVoice&text=' + '"(' + input + ')"', fetchSettings).then((response) => {
+    return fetch('https://watson-api-explorer.mybluemix.net/text-to-speech/api/v1/synthesize?accept=audio%2Fogg%3Bcodecs%3Dopus&voice=en-US_MichaelVoice&text=' + input, fetchSettings).then((response) => {
       return response;
     });
   },
