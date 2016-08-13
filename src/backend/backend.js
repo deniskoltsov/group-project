801aa24f3ffa02d12c98e7e40cb72187b44f29c2
Denis Koltsov <denis_koltsov@yahoo.com>
2016-08-13 08:39:31 -0400

    err

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -2,29 +2,22 @@ const express = require('express'),
       app = express(),
       cors = require('cors'),
       request = require('request'),
-      bodyParser = require('body-parser'),
-      watson = require('watson-developer-cloud'),
-      path = require('path');
+      bodyParser = require('body-parser');
+      watson = require('watson-developer-cloud');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use(express.static(__dirname + '/public'))
-
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-)
-
 app.post('/searchtrack', (req, res) => {
-  const url = 'https://api.musixmatch.com/ws/1.1/matcher.track.get?q_track=' + req.body.data.song + '&q_artist=' + req.body.data.artist + '&apikey=262ed6e47e180f3fe28f5b2e621a5a3f'
+  const url = 'http://api.musixmatch.com/ws/1.1/matcher.track.get?q_track=' + req.body.data.song + '&q_artist=' + req.body.data.artist + '&apikey=262ed6e47e180f3fe28f5b2e621a5a3f'
   request(url, (err, response, body) => {
    res.send(body);
   })
 })
 
 app.post('/searchlyrics', (req, res) => {
-  const url = 'https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + req.body.data.track_id + '&apikey=262ed6e47e180f3fe28f5b2e621a5a3f';
+  const url = 'http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + req.body.data.track_id + '&apikey=262ed6e47e180f3fe28f5b2e621a5a3f';
   request(url, (err, response, body) => {
     res.send(body);
   })
@@ -40,23 +33,14 @@ app.post('/urban-translation', (req, res) => {
 
 app.post('/analyze', (req, res) => {
   const url = 'https://watson-api-explorer.mybluemix.net/tone-analyzer/api/v3/tone?version=2016-05-19&text=' + req.body.data.lyrics
-  console.log('ANALYZE LYRICS URL LINE 43:', url)
+  console.log(url)
   request(url, (err, response, body) => {
     res.send(body);
-    console.log('ANALYZE RESPONSE LINE 46:', response)
+    console.log(response)
   })
 })
 
-app.post('/speech', (req, res) => {
-  const url = 'https://stream.watsonplatform.net/speech-to-text/api' + req.body.data.input
-  console.log('BACKENED URL, LINE 52:', url)
-  request(url, (err, response, body) => {
-    res.send(body);
-    console.log('BACKENED RESPONSE, LINE 55:', response)
-  })
-})
 
-PORT = process.env.PORT || 80;
-app.listen(PORT);
+app.listen(5000);
 
-console.log('Express server started on port', PORT);
+console.log('Express server started on port 5000');
