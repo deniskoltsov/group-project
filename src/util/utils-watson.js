f08fd68847b3079b6091736cb223cf45d3450508
Hiro Narita <hiro.narita@gmail.com>
2016-08-13 11:36:55 -0400

    rap god

--- src/util/utils-watson.js
+++ src/util/utils-watson.js
@@ -8,7 +8,8 @@ export default {
         input: input
       },
       headers: {
-        'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+        'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Access-Control-Allow-Origin': "*"
       }
     }
     return fetch('https://watson-api-explorer.mybluemix.net/text-to-speech/api/v1/synthesize?accept=audio%2Fogg%3Bcodecs%3Dopus&voice=en-US_MichaelVoice&text=' + input, fetchSettings).then((response) => {
