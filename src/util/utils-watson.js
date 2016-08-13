e7832819a09c96b5b1a7c42591b85bc09fb80ed6
Denis Koltsov <denis_koltsov@yahoo.com>
2016-08-12 23:37:33 -0400

    errrr

--- src/util/utils-watson.js
+++ src/util/utils-watson.js
@@ -3,17 +3,12 @@ import axios from 'axios';
 export default {
   translate: function(input) {
     const axiosSettings = {
-      method: 'POST',
+      method: 'GET',
       data: {
         input: input
-      },
-      credentials: {
-        "url": "https://watson-api-explorer.mybluemix.net/text-to-speech/api/v1/synthesize?accept=audio%2Fogg%3Bcodecs%3Dopus&voice=en-US_MichaelVoice&text=",
-        "password": "k73aVGARYwBn",
-        "username": "c952111d-55d4-4d1f-95f5-20b77bc9b4cc"
       }
     }
-    return axios.post('https://immense-beach-20112.herokuapp.com/speech', axiosSettings).then((response) => {
+    return axios.get('https://immense-beach-20112.herokuapp.com/speech', axiosSettings).then((response) => {
       return response;
     });
   },
