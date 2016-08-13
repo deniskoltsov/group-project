e5efd61aa8eb5d8cf8d9879765395330695f765a
Denis Koltsov <denis_koltsov@yahoo.com>
2016-08-13 10:40:43 -0400

    more conflict fixes

--- src/components/Analyze.js
+++ src/components/Analyze.js
@@ -6,7 +6,6 @@ class Analyze extends Component {
 render() {
   const emotions = this.props.tonesObject.map((object, i)  => {
       return <li className='elements' key={i}><span className='name'>{object.tone_name}</span>: <span className='score'>{object.score}</span></li>
-      console.log({this.props});
   })
   return (
     <div className="analyze-component">
