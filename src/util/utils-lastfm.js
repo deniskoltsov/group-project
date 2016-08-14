97146c56cd8f66711ee0d10e124230381f93bf62
Maggie Merrell <maggiemerrell31@gmail.com>
2016-08-14 12:57:47 -0400

    last fm error

--- src/util/utils-lastfm.js
+++ src/util/utils-lastfm.js
@@ -1,8 +1,8 @@
 
 export default {
-  const LASTFM_KEY = process.env.LASTFM_KEY;
-  console.log('YOUTUBE', YOUTUBE_KEY);
   getInfo: function (artist) {
+    const LASTFM_KEY = process.env.LASTFM_KEY;
+    console.log('lastfm', LASTFM_KEY);
     return fetch('https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=' + LASTFM_KEY + '&format=json').then(res => {
       return res.json();
     })
