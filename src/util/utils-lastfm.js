11c65ed167f18807a30cd513b6860f9455e4213f
Maggie Merrell <maggiemerrell31@gmail.com>
2016-08-14 11:02:46 -0400

    lat fm key hidden

--- src/util/utils-lastfm.js
+++ src/util/utils-lastfm.js
@@ -1,6 +1,8 @@
 export default {
   getInfo: function (artist) {
-    return fetch('https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=9dbc1f0658397d0faac1478d78cfd277&format=json').then(res => {
+    const LAST_FM_KEY = process.env.LAST_FM_KEY;
+    console.log('last fM key', LAST_FM_KEY);
+    return fetch('https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=' + LAST_FM_KEY +'&format=json').then(res => {
       return res.json();
     })
   }
