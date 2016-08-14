64db59d2e9ba5ef3102a4b11990c800c3495e70d
Maggie Merrell <maggiemerrell31@gmail.com>
2016-08-14 13:08:14 -0400

    oooh

--- src/util/utils-lastfm.js
+++ src/util/utils-lastfm.js
@@ -1,8 +1,6 @@
 
 export default {
   getInfo: function (artist) {
-    const LASTFM_KEY = process.env.LASTFM_KEY;
-    console.log('lastfm', LASTFM_KEY);
     return fetch('https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=9dbc1f0658397d0faac1478d78cfd277&format=json').then(res => {
       return res.json();
     })
